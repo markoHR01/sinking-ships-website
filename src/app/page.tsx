62448f03cff55e3129d324ad66701bfec2219634
markoHR01 <marko.marko2adm@gmail.com>
2025-09-21 14:39:46 +0200

    Add: app/layout, app/page

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+export default function HomePage() {
+    return (
+        <main className="p-8">
+          <h1 className="text-4xl font-bold mb-4">Potapanje Brodova</h1>
+          <p className="mb-6">Potapanje Brodova je video igra za 2 igrača.</p>
+
+          <button className="bg-blue-500 text-white p-2 mr-1">Preuzmi za Linux</button>
+          <button className="bg-blue-500 text-white p-2 mr-1">Preuzmi za Windows</button>
+        </main>
+    )
+}
