a80df044da757fc64929cf058d9641ae197239a5
markoHR01 <marko.marko2adm@gmail.com>
2025-09-22 18:21:08 +0200

    Implemented: app/page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,70 @@
+import Image from "next/image"
+
 export default function HomePage() {
     return (
-        <main className="p-8">
-          <h1 className="text-4xl font-bold mb-4">Potapanje Brodova</h1>
-          <p className="mb-6">Potapanje Brodova je video igra za 2 igrača.</p>
+        <main className="py-10 flex justify-center items-center">
 
-          <button className="bg-blue-500 text-white p-2 mr-1">Preuzmi za Linux</button>
-          <button className="bg-blue-500 text-white p-2 mr-1">Preuzmi za Windows</button>
+          {/* Title - Short Text - Download */}
+          <div className="flex flex-col items-center text-center space-y-4 p-2 mx-auto">
+            <h1 className="text-6xl font-bold mb-12">
+              <span>Potapanje</span><br />
+              <span>Brodova</span>
+            </h1>
+
+            <p className="mb-8 max-w-md">
+              Potapanje Brodova je video igra koju igraju 2 igrača
+              u stvarnom vremenu. Za igru je potreban pristup internetu.
+              Inspiracija za ovu igru je klasična igra Battleships (1967).
+            </p>
+
+            <p className="mb-6 max-w-sm">
+              Izvorni kod ove vlastite implementacije dostupan je na{" "}
+              <a
+                href="https://github.com/markoHR01"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-blue-600 hover:underline"
+              >
+                github.com/markoHR01
+              </a>
+            </p>
+
+            <p className="mb-10 max-w-md">
+              Više o pravilima igre možete pročitati{" "}
+              <a
+                href="/pravila"
+                className="text-blue-600 hover:underline"
+              >
+                ovdje
+              </a>.
+            </p>
+
+            <a
+              href="/preuzmi/sinking-ships-linux.tar.gz"
+              className="bg-green-600 hover:bg-green-700 text-white px-4 py-2 rounded"
+            >
+              Preuzmi za Linux
+            </a>
+
+            <a
+              href="/preuzmi/sinking-ships-win32.zip"
+              className="bg-blue-600 hover:bg-blue-700 text-white px-4 py-2 rounded"
+            >
+              Preuzmi za Windows
+            </a>
+          </div>
+
+          {/* Picture : Win */}
+          <div className="flex-shring-0 mx-auto">
+            <Image
+              src="/slike/win.png"
+              alt="Igra - Pobjeda"
+              width={1067}
+              height={832}
+              className="rounded"
+            />
+          </div>
+
         </main>
     )
 }
